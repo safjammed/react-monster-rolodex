7f516729413a9e0ce9a2b62e853488fd59c60848
safjammed <safayatjamil27@gmail.com>
2019-12-28 01:16:35 +0600

    Updates

---
+++ precache-manifest.d336f4502ca089f612ce1eba202d42e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edaa212558a49682f1743ec2335c4b0",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "8844f6daac4522e8fad4",
+    "url": "/react-monster-rolodex/static/css/main.91a5f333.chunk.css"
+  },
+  {
+    "revision": "8b62ed86ccad16d93f67",
+    "url": "/react-monster-rolodex/static/js/2.754ae8e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monster-rolodex/static/js/2.754ae8e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8844f6daac4522e8fad4",
+    "url": "/react-monster-rolodex/static/js/main.134401d6.chunk.js"
+  },
+  {
+    "revision": "4d410b86e597eb132072",
+    "url": "/react-monster-rolodex/static/js/runtime-main.f479cb15.js"
+  }
+]);
